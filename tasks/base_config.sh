626fd175f7c14eef22c65c18afabfca76b2d6ccf
Takács Zoltán Gábor <zozitak@gmail.com>
2023-10-27 09:20:09 +0200

    Update base_config.sh

--- tasks/base_config.sh
+++ tasks/base_config.sh
@@ -1,6 +1,5 @@
 #!usr/bin/bash
 git clone --bare https://github.com/zozitak/.dotfiles.git $HOME/.dotfiles
 echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.bashrc
-source $HOME/.bashrc
-dotfiles checkout -f
-dotfiles config --local status.showUntrackedFiles no
+/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout -f
+/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
