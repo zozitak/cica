53b9c146f35432063d88eaa63463b804c516d1d7
Takács Zoltán Gábor <zozitak@gmail.com>
2023-10-31 12:51:30 +0100

    Update base_config.sh

--- tasks/base_config.sh
+++ tasks/base_config.sh
@@ -1,7 +1,14 @@
 #!usr/bin/bash
 set -euo pipefail
+
+echo "Cloning dotfiles..." 
 git clone --bare https://github.com/zozitak/.dotfiles.git $HOME/.dotfiles
+echo "Deploy dotfiles..." 
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout -f
+echo "Set config: status.showUntrackedFiles no" 
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
+echo "Set aliases..." 
 echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.bashrc
+echo "Refresh sources" 
 exec bash
+tmux source-file $HOME/.tmux.conf
