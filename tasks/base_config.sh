782a7498aad60f0028075219a0c954861e6f48bf
Takács Zoltán Gábor <zozitak@gmail.com>
2023-10-31 12:46:50 +0100

    Update base_config.sh

--- tasks/base_config.sh
+++ tasks/base_config.sh
@@ -1,4 +1,5 @@
 #!usr/bin/bash
+set -euo pipefail
 git clone --bare https://github.com/zozitak/.dotfiles.git $HOME/.dotfiles
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout -f
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
