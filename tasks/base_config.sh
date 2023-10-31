c8221a67b1632102b5f2ea7ccf907a1b93a04616
Takács Zoltán Gábor <zozitak@gmail.com>
2023-10-31 12:52:39 +0100

    Update base_config.sh

--- tasks/base_config.sh
+++ tasks/base_config.sh
@@ -9,6 +9,3 @@ echo "Set config: status.showUntrackedFiles no"
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
 echo "Set aliases..." 
 echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.bashrc
-echo "Refresh sources" 
-exec bash
-tmux source-file $HOME/.tmux.conf
