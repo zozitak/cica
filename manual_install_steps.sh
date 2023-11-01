4c6aaa9887cc1f5c6cbaf1592644289750feb264
Takács Zoltán Gábor <zozitak@gmail.com>
2023-11-01 22:33:37 +0100

    Update manual_install_steps.sh

--- manual_install_steps.sh
+++ manual_install_steps.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#install ansible 
-sudo apt install ansible
-#snap collection for ansible
-ansible-galaxy collection install community.general
-#ansible-playbook devenv.yml --ask-become-pass
+#apt install git ansible
+#git clone http://www.github.com/zozitak/cica
+#ansible-galaxy collection install community.general
+#ansible-playbook setup.yml --ask-become-pass
